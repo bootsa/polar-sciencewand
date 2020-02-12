259cd4c1b78347800dfe84f203f0970ac9bfef8a
Steph Tyszka <steph@nuclio.org>
2020-02-12 10:58:56 +0100

    backend: move User list file and add Ideas lists

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,16 +1,22 @@
 require('dotenv-extended').load();
 
 const { Keystone } = require('@keystonejs/keystone');
+
 const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
-const { Text, Checkbox, Password } = require('@keystonejs/fields');
+
 const { GraphQLApp } = require('@keystonejs/app-graphql');
 const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+
+const UsersSchema = require('./lists/Users');
+const BigIdeaSchema = require('./lists/BigIdeas');
+const IntermediateIdeaSchema = require('./lists/IntermediateIdeas');
+const BasicIdeaSchema = require('./lists/BasicIdeas');
+
 const initialiseData = require('./initial-data');
 
 const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
 
-const PROJECT_NAME = "Polar Star - Science Wand - Backend";
-
+const PROJECT_NAME = "Polar Star: Science Wand";
 
 const keystone = new Keystone({
   name: PROJECT_NAME,
@@ -20,41 +26,10 @@ const keystone = new Keystone({
   onConnect: initialiseData,
 });
 
-// Access control functions
-const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
-const userOwnsItem = ({ authentication: { item: user } }) => {
-  if (!user) {
-    return false;
-  }
-  return { id: user.id };
-};
-const userIsAdminOrOwner = auth => {
-  const isAdmin = access.userIsAdmin(auth);
-  const isOwner = access.userOwnsItem(auth);
-  return isAdmin ? isAdmin : isOwner;
-};
-const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
-
-keystone.createList('User', {
-  fields: {
-    name: { type: Text },
-    email: {
-      type: Text,
-      isUnique: true,
-    },
-    isAdmin: { type: Checkbox },
-    password: {
-      type: Password,
-    },
-  },
-  access: {
-    read: access.userIsAdminOrOwner,
-    update: access.userIsAdminOrOwner,
-    create: access.userIsAdmin,
-    delete: access.userIsAdmin,
-    auth: true,
-  },
-});
+keystone.createList('User', UsersSchema);
+keystone.createList('BigIdea', BigIdeaSchema);
+keystone.createList('IntermediateIdea', IntermediateIdeaSchema);
+keystone.createList('BasicIdea', BasicIdeaSchema);
 
 const authStrategy = keystone.createAuthStrategy({
   type: PasswordAuthStrategy,
---
+++ packages/backend/lists/IntermediateIdeas.js
@@ -0,0 +1,10 @@
+const { Relationship, Text } = require('@keystonejs/fields');
+const { Markdown } = require('@keystonejs/fields-markdown');
+
+module.exports = {
+  fields: {
+    name: { type: Text, isRequired: true },
+    description: { type: Markdown },
+    parent: { type: Relationship, ref: 'BigIdea' }
+  }
+}
